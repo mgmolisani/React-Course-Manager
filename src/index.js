b1c5e8f62132104e0b2281925608f6eebd0ce0f1
mgmolisani <molisani.m@husky.neu.edu>
2018-05-30 10:15:55 -0400

    Added npm packages and dependencies

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CourseManager from "./containers/CourseManager";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/font-awesome/css/font-awesome.min.css'
 
 ReactDOM.render(
-    <h1>Hello, world!</h1>,
+    <CourseManager/>,
     document.getElementById('root')
 );
