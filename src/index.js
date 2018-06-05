0801418773902c0c167f4ffe3792b05576c56198
mgmolisani <molisani.m@husky.neu.edu>
2018-06-04 22:44:32 -0400

    Updated Index with styling

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CourseManager from "./containers/CourseManager";
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../node_modules/font-awesome/css/font-awesome.min.css'
+import './course-manager.style.client.css'
 
+/**
+ * The main view.
+ */
 ReactDOM.render(
     <CourseManager/>,
     document.getElementById('root')
